cdb99a6d2268fd0dce3ff916b4b8486e62884961
Mithil Gaonkar <mithilgaonkar01@gmail.com>
2022-09-20 17:26:08 +0530

    Time: 1320 ms (47.08%), Space: 14 MB (58.08%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,34 +1,32 @@
 class Solution:
-    #T(n) = O(n**2) and S(n) = O(n**2)
+    #T(n) = O(n**2) and S(n) = O(1)
+    def checkaroundi(self,left,right,s,n,palinstr,max_len):
+        
+        while left >= 0 and right < n and s[left] == s[right]:
+            if max_len < right - left + 1:
+                max_len = right - left + 1
+                palinstr = s[left: left + (right - left + 1)]
+            
+            left -= 1
+            right += 1
+            
+        return max_len,palinstr
     
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
         
-        dp = [[0]*n for _ in range(n)]
-        max_len = 0
-        result = " "
+        if n == 1:
+            return s
         
-        #len=1 substring
-        for i in range(n):
-            dp[i][i] = 1
-            max_len = 1
-            result = s[i]
+        max_len = 1
+        #string len is 1 
+        palinstr = s[0] 
         
-        #len=2 substring
-        for i in range(n-1):
-            if s[i] == s[i+1]:
-                dp[i][i+1] = 1
-                max_len = 2
-                result = s[i:i+2]
+        #maintain two pointers, left and right towards either ends of i
+        for i in range(1,n):
+            #if substring is even len
+            max_len,palinstr = self.checkaroundi(i-1,i,s,n,palinstr,max_len)
+            #if substring is odd len
+            max_len,palinstr = self.checkaroundi(i-1,i+1,s,n,palinstr,max_len)
         
-        #len = 3 to n substring
-        for i in range(2,n):
-            for j in range(i-1):
-                #check first-last char match and in dp table check substr(from                       #second - secondlast char matches)
-                if s[i] == s[j] and dp[j+1][i-1]:
-                    dp[j][i] = 1
-                    if max_len < i-j+1:
-                        result = s[j:i+1]
-                        max_len = i - j + 1
-        return result
-            
+        return palinstr
